00a92fa80d762f106f65c21e99bec0c4df607376
Andrew Binnie <binnieandrew@gmail.com>
2020-02-12 22:08:41 +0000

    adding find whiskies by year and distilleries by region

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRepositoryCustom {
+
+    public List<Whisky> getByYear(int year);
+
+    public List<Whisky> findByDistilleryIdAndAge(int id, int age);
+}
